145cd2c347dc875b93038ad133871adc98d70350
antj54 <anthonyjimenez592@gmail.com>
2022-07-31 13:24:52 -0400

    Add left and right movement to projectile div

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 let playButton = document.getElementById('play');
 
 function play() {
-  document.addEventListener('keypress', up);
+  document.addEventListener('keydown', up); //executes up()
   down();
-  playButton.removeEventListener('mousedown', play );
+  playButton.removeEventListener('mousedown', play ); //remove play button function on execution
   playButton.style.cursor = 'default';
-}
+};
 
 playButton.addEventListener('mousedown', play);
 
@@ -20,13 +20,38 @@ document.addEventListener('keydown', event => {
   if (event.code === 'Space') {
     ball.style.bottom = '910px';
   }
-})
-resetButton.addEventListener("mousedown", down);
-}
+});
+resetButton.addEventListener("mousedown", down); //add reset button function on up()
+};
 
 function down() {
     ball.style.bottom = "0px";
-    
-  }
 
-resetButton.addEventListener("mousedown", down);
+};
+
+
+//Setting arrow key functionality
+let projectile = document.getElementById('projectile');
+let moveBy = 10;
+
+
+window.addEventListener('load', () => {
+  projectile.style.position = 'relative';
+  projectile.style.left = 0;
+});
+
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'ArrowLeft':
+      projectile.style.left = parseInt(projectile.style.left) - moveBy + 'px';
+      break;
+    case 'ArrowRight': 
+      projectile.style.left = parseInt(projectile.style.left) + moveBy + 'px';
+      break;
+  }
+})
+
+
+
+
+
