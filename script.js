3800ffdc5b1d219f281d26b7900614fe40f6efe1
antj54 <anthonyjimenez592@gmail.com>
2022-07-31 11:16:49 -0400

    Added play button and functionality in JS

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Add keypress event listener to play button
+let playButton = document.getElementById('play');
+
+function play() {
+  document.addEventListener('keypress', up);
+  down();
+}
+
+playButton.addEventListener('mousedown', play);
+
 // Defining ball variable and function to launch and retrieve ball
 let ball = document.getElementById('ball');
 let resetButton = document.getElementById('reset'); 
@@ -6,10 +16,16 @@ function up() {
     ball.style.bottom = '910px';  
 }
 
+document.addEventListener('keydown', event => {
+  if (event.code === 'Space') {
+    console.log('Space pressed')
+  }
+})
+
 function down() {
     ball.style.bottom = "0px";
     
   }
 
-document.addEventListener('keypress', up);
+
 resetButton.addEventListener("mousedown", down);
