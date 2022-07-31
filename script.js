c66318694e29a3a95482d67868987f585d417869
antj54 <anthonyjimenez592@gmail.com>
2022-07-31 11:34:38 -0400

    Changed some button style stuff on play()

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-// Add keypress event listener to play button
+// Add event listener to play button
 let playButton = document.getElementById('play');
 
 function play() {
   document.addEventListener('keypress', up);
   down();
+  playButton.removeEventListener('mousedown', play );
+  playButton.style.cursor = 'default';
 }
 
 playButton.addEventListener('mousedown', play);
+
 
 // Defining ball variable and function to launch and retrieve ball
 let ball = document.getElementById('ball');
 let resetButton = document.getElementById('reset'); 
 
 function up() {
-    ball.style.bottom = '910px';  
-}
-
 document.addEventListener('keydown', event => {
   if (event.code === 'Space') {
-    console.log('Space pressed')
+    ball.style.bottom = '910px';
   }
 })
+resetButton.addEventListener("mousedown", down);
+}
 
 function down() {
     ball.style.bottom = "0px";
     
   }
-
 
 resetButton.addEventListener("mousedown", down);
